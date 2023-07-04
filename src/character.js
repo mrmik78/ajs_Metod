fd355cf18a79e3cb22965863d0a9f1a37b1dcd1b
МС 21 <m.reutsky@mail.ru>
2023-07-04 21:12:58 +0300

    second change character

--- src/character.js
+++ src/character.js
@@ -1,12 +1,10 @@
 export default class Character {
   constructor(name, type) {
-    // eslint-disable-next-line no-empty
     if (typeof name === 'string' && name.length > 2 && name.length < 10) {
-
+      this.name = name;
     } else {
       throw new Error('Длина имени должна быть не менее 2-ух и не более 10-ти символов');
     }
-    this.name = name;
     this.type = type;
     this.health = 100;
     this.level = 1;
